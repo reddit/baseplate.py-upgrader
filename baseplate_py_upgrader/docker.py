a955e2dd8f40e1fac37003798b7119ef0920e379
Neil Williams <neil@reddit.com>
2021-03-08 09:42:47 -0800

    2.0: Fix handling of -artifactory docker images

--- baseplate_py_upgrader/docker.py
+++ baseplate_py_upgrader/docker.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 IMAGE_RE = re.compile(
-    r"/baseplate-py:(?P<version>[0-9.]+(\.[0-9]+)?)-py(?P<python>[23]\.[0-9]+)-(?P<distro>(bionic|buster))(?P<dev>-dev)?"
+    r"/baseplate-py:(?P<version>[0-9.]+(\.[0-9]+)?)-py(?P<python>[23]\.[0-9]+)-(?P<distro>(bionic|buster))(?P<repo>-artifactory)?(?P<dev>-dev)?"
 )
 
 
@@ -22,14 +22,17 @@ def upgrade_docker_image_references_in_file(target_series: str, filepath: Path) 
 
     force_distro = None
     force_dev = False
+    force_repo = None
     if major == "2":
         force_distro = "buster"
         force_dev = True
+        force_repo = ""
 
     def replace_docker_image_reference(m: Match[str]) -> str:
         distro = force_distro or m["distro"]
+        repo = force_repo if force_repo is not None else m["repo"]
         dev = "-dev" if force_dev else m["dev"]
-        return f"/baseplate-py:{image_series}-py{m['python']}-{distro}{dev or ''}"
+        return f"/baseplate-py:{image_series}-py{m['python']}-{distro}{repo or ''}{dev or ''}"
 
     file_content = filepath.read_text()
     changed = IMAGE_RE.sub(replace_docker_image_reference, file_content, re.MULTILINE)
